6b68df15dcc9359dd5a153e274a8df1a652bfa1a
Elfin Git <musongoletracy@gmail.com>
2022-12-08 13:09:17 +0200

    add tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  user1 = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+  subject do
+    Post.new(title: 'My post', text: 'Hello! My name is King shukur.', author: user1, comments_counter: 3,
+             likes_counter: 2)
+  end
+
+  before { user1.save subject.save }
+
+  it 'title should be present' do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'likes counter should be an integer' do
+    subject.likes_counter = 'a'
+    expect(subject).to_not be_valid
+  end
+
+  it 'post length should be more than 10 charachters' do
+    subject.text = 'Hello'
+    expect(subject).to_not be_valid
+  end
+
+  it 'post should belong to an author' do
+    subject.author = nil
+    expect(subject).to_not be_valid
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+
+  before { subject.save }
+
+  it 'name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'post counter should be integer' do
+    subject.posts_counter = 'a'
+    expect(subject).to_not be_valid
+  end
+
+  it 'likes counter should be integer' do
+    subject.likes_counter = 'a'
+    expect(subject).to_not be_valid
+  end
 end
