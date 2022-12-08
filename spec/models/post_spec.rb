44cecfaeccbe6f608eebab93fa765ace4b8d9a99
Elfin Git <musongoletracy@gmail.com>
2022-12-08 20:40:52 +0200

    update tests and fix linters

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,11 +3,12 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   user1 = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
   subject do
-    Post.new(title: 'My post', body: 'Hello! My name is King shukur.', author: user1, comments_counter: 3,
+    Post.new(title: 'My post', body: 'Hello! My name is King shukur.', author: user1, comments_counter: 1,
              likes_counter: 2)
   end
 
-  before { user1.save subject.save }
+  before { user1.save }
+  before { subject.save }
 
   it 'title should be present' do
     subject.title = nil
@@ -16,11 +17,6 @@ RSpec.describe Post, type: :model do
 
   it 'likes counter should be an integer' do
     subject.likes_counter = 'a'
-    expect(subject).to_not be_valid
-  end
-
-  it 'post should belong to an author' do
-    subject.author = nil
     expect(subject).to_not be_valid
   end
 end
