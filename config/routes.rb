608bfabb63a1cc742a2423cf25ccaf843bae18f5
Elfin Git <musongoletracy@gmail.com>
2022-12-12 13:09:32 +0200

    generate controllers for posts and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/users', to: 'users#index'
+  get 'users/:id', to: "users#show", as: 'user'
+  get 'users/:author_id/posts', to: 'posts#index', as: 'user_posts'
+  get 'users/:author_id/posts/:id', to: 'posts#show', as: 'user_post'
 end
