329ca6db8379719ebcd867da09037f5b03db9e09
Elfin Git <musongoletracy@gmail.com>
2022-12-13 11:18:55 +0200

    add tests for controllers

--- Gemfile
+++ Gemfile
@@ -6,6 +6,9 @@ ruby '3.1.3'
 # Rubocop for linting
 gem 'rubocop', '>= 1.0', '< 2.0'
 
+# for controller testing
+gem 'rails-controller-testing'
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem 'rails', '~> 7.0.4'
 
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    before (:example) { get '/users/:user_id/posts' }
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template('index')
+      expect(response.body).to include('all posts are found here')
+    end
+  end
+
+  describe 'GET /show' do
+    it 'returns http success' do
+      get '/users/1/posts/1'
+      expect(response).to have_http_status(:success) 
+      expect(response).to render_template('show')
+      expect(response.body).to include('post page')
+   end
   end
 end
