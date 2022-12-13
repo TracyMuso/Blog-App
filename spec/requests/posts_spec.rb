5e6662498e088d4475b8dc9b4a349034c9011f77
Elfin Git <musongoletracy@gmail.com>
2022-12-13 11:20:32 +0200

    fix linter errors

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    before (:example) { get '/users/:user_id/posts' }
+    before(:example) { get '/users/:user_id/posts' }
     it 'returns http success' do
       expect(response).to have_http_status(:success)
       expect(response).to render_template('index')
@@ -13,9 +13,9 @@ RSpec.describe 'Posts', type: :request do
   describe 'GET /show' do
     it 'returns http success' do
       get '/users/1/posts/1'
-      expect(response).to have_http_status(:success) 
+      expect(response).to have_http_status(:success)
       expect(response).to render_template('show')
       expect(response.body).to include('post page')
-   end
+    end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    before (:example) { get users_path(1) }
+    before(:example) { get users_path(1) }
     it 'returns http success' do
       expect(response).to have_http_status(:success)
       expect(response).to render_template('index')
-     expect(response.body).to include('Hello there')
+      expect(response.body).to include('Hello there')
     end
   end
 
   describe 'GET /show' do
-    before (:example) { get user_path(1) }
+    before(:example) { get user_path(1) }
     it 'returns http success' do
       expect(response).to have_http_status(:success)
       expect(response).to render_template('show')
