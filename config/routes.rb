48077d84d245448c4cbbdd3ce9f807287827ad08
Danny <barakadan421@gmail.com>
2022-12-23 12:12:51 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,16 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  root "users#index"
-
-  resources :users, only: [:show] do
-    resources :posts, only: [:new, :create, :index, :show] do
-      resources :comments, only: [:new, :create]
-      resources :likes, only: [:create]
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'users#index', as: :authenticated_root
     end
+    unauthenticated :user do
+      root :to => 'devise/registrations#new', as: :unauthenticated_root
+    end
+  end
+
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
   end
 end
